a1ba67ed161c1a5af1f23ebe73e2735c09db5fe4
bigfooted <bigfootedrockmidget@hotmail.com>
2023-10-02 22:03:57 +0200

    changed pyqt5 to tk

--- matplotlib_realtime.py
+++ matplotlib_realtime.py
@@ -91,124 +91,133 @@ state.keep_updating = True
 state.countdown = 1000
 
 
-# file_browser_ui.py
+# tk file browser
+from tkinter import filedialog, Tk
+# Keep track of the currently selected directory
+state.selected_dir = None
+root = Tk()
+# Ensure the tkinter main window is hidden
+root.withdraw()
+# Ensure that the file browser will appear in front on Windows
+root.wm_attributes("-topmost", 1)
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+# qt5 file_browser_ui.py
+#from PyQt5.QtCore import *
+#from PyQt5.QtGui import *
+#from PyQt5.QtWidgets import *
 
 import sys
 
 # A simple widget consisting of a QLabel, a QLineEdit and a
 # QPushButton. The class could be implemented in a separate
 # script called, say, file_browser.py
-class FileBrowser(QWidget):
+# class FileBrowser(QWidget):
 
-    OpenFile = 0
+#     OpenFile = 0
 
-    def __init__(self, title, mode=OpenFile):
-        QWidget.__init__(self)
-        layout = QHBoxLayout()
-        self.setLayout(layout)
-        self.browser_mode = mode
-        self.filter_name = 'All files (*.*)'
-        self.dirpath = QDir.currentPath()
-        self.filepaths = []
+#     def __init__(self, title, mode=OpenFile):
+#         QWidget.__init__(self)
+#         layout = QHBoxLayout()
+#         self.setLayout(layout)
+#         self.browser_mode = mode
+#         self.filter_name = 'All files (*.*)'
+#         self.dirpath = QDir.currentPath()
+#         self.filepaths = []
 
-        self.label = QLabel()
-        self.label.setText(title)
-        self.label.setFixedWidth(65)
-        self.label.setFont(QFont("Arial",weight=QFont.Bold))
-        self.label.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
-        layout.addWidget(self.label)
+#         self.label = QLabel()
+#         self.label.setText(title)
+#         self.label.setFixedWidth(65)
+#         self.label.setFont(QFont("Arial",weight=QFont.Bold))
+#         self.label.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+#         layout.addWidget(self.label)
 
-        self.lineEdit = QLineEdit(self)
-        self.lineEdit.setFixedWidth(180)
+#         self.lineEdit = QLineEdit(self)
+#         self.lineEdit.setFixedWidth(180)
 
-        layout.addWidget(self.lineEdit)
+#         layout.addWidget(self.lineEdit)
 
-        self.button = QPushButton('Search')
-        self.button.clicked.connect(self.getFile)
-        layout.addWidget(self.button)
-        layout.addStretch()
-    #--------------------------------------------------------------------
-    def setMode(mode):
-        self.mode = mode
-    #--------------------------------------------------------------------
-    # For example,
-    #    setFileFilter('Images (*.png *.xpm *.jpg)')
-    def setFileFilter(text):
-        self.filter_name = text
-    #--------------------------------------------------------------------
-    def setDefaultDir(path):
-        self.dirpath = path
-    #--------------------------------------------------------------------
-    def getFile(self):
-        self.filepaths = []
+#         self.button = QPushButton('Search')
+#         self.button.clicked.connect(self.getFile)
+#         layout.addWidget(self.button)
+#         layout.addStretch()
+#     #--------------------------------------------------------------------
+#     def setMode(mode):
+#         self.mode = mode
+#     #--------------------------------------------------------------------
+#     # For example,
+#     #    setFileFilter('Images (*.png *.xpm *.jpg)')
+#     def setFileFilter(text):
+#         self.filter_name = text
+#     #--------------------------------------------------------------------
+#     def setDefaultDir(path):
+#         self.dirpath = path
+#     #--------------------------------------------------------------------
+#     def getFile(self):
+#         self.filepaths = []
 
-        if self.browser_mode == FileBrowser.OpenFile:
-            self.filepaths.append(QFileDialog.getOpenFileName(self, caption='Choose File',
-                                                    directory=self.dirpath,
-                                                    filter=self.filter_name)[0])
-        if len(self.filepaths) == 0:
-            return
-        elif len(self.filepaths) == 1:
-            self.lineEdit.setText(self.filepaths[0])
-        else:
-            self.lineEdit.setText(",".join(self.filepaths))
-    #--------------------------------------------------------------------
-    def setLabelWidth(self, width):
-        self.label.setFixedWidth(width)
-    #--------------------------------------------------------------------
-    def setlineEditWidth(self, width):
-        self.lineEdit.setFixedWidth(width)
-    #--------------------------------------------------------------------
-    def getPaths(self):
-        return self.filepaths
-#-------------------------------------------------------------------
+#         if self.browser_mode == FileBrowser.OpenFile:
+#             self.filepaths.append(QFileDialog.getOpenFileName(self, caption='Choose File',
+#                                                     directory=self.dirpath,
+#                                                     filter=self.filter_name)[0])
+#         if len(self.filepaths) == 0:
+#             return
+#         elif len(self.filepaths) == 1:
+#             self.lineEdit.setText(self.filepaths[0])
+#         else:
+#             self.lineEdit.setText(",".join(self.filepaths))
+#     #--------------------------------------------------------------------
+#     def setLabelWidth(self, width):
+#         self.label.setFixedWidth(width)
+#     #--------------------------------------------------------------------
+#     def setlineEditWidth(self, width):
+#         self.lineEdit.setFixedWidth(width)
+#     #--------------------------------------------------------------------
+#     def getPaths(self):
+#         return self.filepaths
+# #-------------------------------------------------------------------
 
-class Demo(QDialog):
-    def __init__(self, parent=None):
-        QDialog.__init__(self, parent)
+# class Demo(QDialog):
+#     def __init__(self, parent=None):
+#         QDialog.__init__(self, parent)
 
-        # Ensure our window stays in front and give it a title
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
-        self.setWindowTitle("File Browsing Dialog")
-        self.setFixedSize(400, 300)
+#         # Ensure our window stays in front and give it a title
+#         self.setWindowFlags(Qt.WindowStaysOnTopHint)
+#         self.setWindowTitle("File Browsing Dialog")
+#         self.setFixedSize(400, 300)
 
-        # Create and assign the main (vertical) layout.
-        vlayout = QVBoxLayout()
-        self.setLayout(vlayout)
+#         # Create and assign the main (vertical) layout.
+#         vlayout = QVBoxLayout()
+#         self.setLayout(vlayout)
 
-        self.fileBrowserPanel(vlayout)
-        vlayout.addStretch()
-        self.addButtonPanel(vlayout)
-        self.show()
-    #--------------------------------------------------------------------
-    def fileBrowserPanel(self, parentLayout):
-        vlayout = QVBoxLayout()
-        self.fileFB = FileBrowser('Open File', FileBrowser.OpenFile)
-        vlayout.addWidget(self.fileFB)
-        vlayout.addStretch()
-        parentLayout.addLayout(vlayout)
-    #--------------------------------------------------------------------
-    def addButtonPanel(self, parentLayout):
-        hlayout = QHBoxLayout()
-        hlayout.addStretch()
+#         self.fileBrowserPanel(vlayout)
+#         vlayout.addStretch()
+#         self.addButtonPanel(vlayout)
+#         self.show()
+#     #--------------------------------------------------------------------
+#     def fileBrowserPanel(self, parentLayout):
+#         vlayout = QVBoxLayout()
+#         self.fileFB = FileBrowser('Open File', FileBrowser.OpenFile)
+#         vlayout.addWidget(self.fileFB)
+#         vlayout.addStretch()
+#         parentLayout.addLayout(vlayout)
+#     #--------------------------------------------------------------------
+#     def addButtonPanel(self, parentLayout):
+#         hlayout = QHBoxLayout()
+#         hlayout.addStretch()
 
-        self.button = QPushButton("OK")
-        self.button.clicked.connect(self.buttonAction)
-        hlayout.addWidget(self.button)
-        parentLayout.addLayout(hlayout)
-    #--------------------------------------------------------------------
-    def buttonAction(self):
-        global history_filename
-        print(self.fileFB.getPaths())
-        history_filename = self.fileFB.getPaths()[0]
-        print("filename=",history_filename)
-        readHistory(history_filename)
+#         self.button = QPushButton("OK")
+#         self.button.clicked.connect(self.buttonAction)
+#         hlayout.addWidget(self.button)
+#         parentLayout.addLayout(hlayout)
+#     #--------------------------------------------------------------------
+#     def buttonAction(self):
+#         global history_filename
+#         print(self.fileFB.getPaths())
+#         history_filename = self.fileFB.getPaths()[0]
+#         print("filename=",history_filename)
+#         readHistory(history_filename)
 
-        self.done(1)
+#         self.done(1)
 
     #--------------------------------------------------------------------
 
@@ -220,11 +229,23 @@ def open_directory_qt():
     #state.selected_dir = filedialog.askopenfilename(**kwargs)
     #print("state.selected_dir=",state.selected_dir)
     print("create qapplication")
-    app = QApplication(sys.argv)
-    demo = Demo() # <<-- Create an instance
-    demo.show()
-    app.exec_()
+    #app = QApplication(sys.argv)
+    #demo = Demo() # <<-- Create an instance
+    #demo.show()
+    #app.exec_()
     print("exit")
+
+
+@ctrl.set("open_directory_tk")
+def open_directory_tk():
+    kwargs = {
+        "title": "Select Directory",
+    }
+    state.selected_dir = filedialog.askopenfilename(**kwargs)
+    print("state.selected_dir=",state.selected_dir)
+    global history_filename
+    history_filename = state.selected_dir
+    readHistory(history_filename)
 
 
 
@@ -574,7 +595,8 @@ with SinglePageWithDrawerLayout(server) as layout:
     with layout.toolbar:
         vuetify.VSpacer()
 
-        with vuetify.VBtn("path",click=ctrl.open_directory_qt):
+        #with vuetify.VBtn("path",click=ctrl.open_directory_qt):
+        with vuetify.VBtn("path",click=ctrl.open_directory_tk):
             vuetify.VIcon("{{solver_icon}}",color="red")
 
         #with vuetify.VBtn("filename",click=ctrl.open_file):
